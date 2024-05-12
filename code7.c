713938b9c2262dda58a52ce0d060dd16f65b8059
student-akriti123 <akritisinha1602@gmail.com>
2024-05-12 22:26:04 +0530

    first commit

---
+++ code7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+int p,SI;
+printf("enter the value p :");
+scanf("%d",&p);
+
+int r;
+printf("enter the value r :");
+scanf("%d",&r);
+
+int t;
+printf("enter the value t :");
+scanf("%d",&t);
+
+//formula
+SI= (p*r*t/100);
+printf("%d",SI);
+
+return 0;
+
+}
