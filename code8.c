6de27d7adf04b31d4cb21f8213c555477b9ec7fa
student-akriti123 <akritisinha1602@gmail.com>
2024-05-12 22:47:25 +0530

    first commit

---
+++ code8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the value of n:");
+scanf("%d",&n);
+
+if(n%2==0)
+{
+printf("this is a even number");
+
+}
+  else
+  {
+    printf("this is a odd number");
+
+  }
+return 0;
+}
