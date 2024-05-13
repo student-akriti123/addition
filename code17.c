9cd0d327d46916468b2d6fb55e579f298b58f1e2
student-akriti123 <akritisinha1602@gmail.com>
2024-05-13 12:53:22 +0530

    first commit

--- code17.c
+++ code17.c
@@ -5,10 +5,10 @@ int main()
 //printf("enter the value n:");
 //scanf("%d",&n);
 
-for(int i=1;i<=10;i++)
+for(int i=1;i<=100;i++)
 {
 
-printf("YES! you can do it\n");
+printf("%d\n",i);
 
 }
 return 0;
