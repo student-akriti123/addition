ca539fdef7d11304d05cd7e46943d9ed8034f6d1
student-akriti123 <akritisinha1602@gmail.com>
2024-05-13 10:55:17 +0530

    first commit

---
+++ code13.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the value of n:");
+scanf("%d",&n);
+
+if(n>99&&n<1000)
+{
+printf("You are greate");
+}
+
+else
+{
+printf("you are smaller");
+}
+
+
+return 0;
+}
