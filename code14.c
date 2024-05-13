f1b16aaa5fe175f4c59cedcd8c87d0f417b24aba
student-akriti123 <akritisinha1602@gmail.com>
2024-05-13 11:01:07 +0530

    first commit

---
+++ code14.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the value of n:");
+scanf("%d",&n);
+
+if(n%3==0&&n%5==0)
+{
+printf("it is divisible ");
+}
+
+else
+{
+printf(" it is not divisible");
+}
+
+
+return 0;
+}
+
