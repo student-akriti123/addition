378399fb61d438bba1e0ddfa1a3ef7f004ce5905
student-akriti123 <akritisinha1602@gmail.com>
2024-05-14 07:24:02 +0530

    first commit

--- code29.c
+++ code29.c
@@ -5,16 +5,12 @@ int n;
 printf("enter the value of n:");
 scanf("%d",&n);
 
-int r=0;
-while(n>0)
+int product=1;
+for(int i=1;i<=n;i++)
 {
-r=r*10;
-r=r+(n%10);
-
-n=n/10;
+product=product*i;
 }
-
-printf("%d",r);
+printf("%d",product);
 
 return 0;
 }
