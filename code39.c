298edd35d3c624be9a818613cbf290602fedd0a8
student-akriti123 <akritisinha1602@gmail.com>
2024-05-18 20:48:51 +0530

    first commit

---
+++ code39.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int r , area;
+printf("enter the value of r:");
+scanf("%d",&r);
+
+//formula
+
+area= 3.14*r*r;
+printf("the area of the circle is:%d",area);
+
+return 0;
+}
